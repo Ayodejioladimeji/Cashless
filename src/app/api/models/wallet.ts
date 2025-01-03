49286cf9bc85349d1c3e384be7fc6c847058a540
Ayodejioladimeji <layobright11@gmail.com>
2025-01-03 14:12:55 +0100

    Created wallet schema

---
+++ src/app/api/models/wallet.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const walletSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true
+        },
+        balance: { type: Number, default: 0 },
+
+    },
+    {
+        timestamps: true,
+    }
+);
+
+let Dataset = mongoose.models.wallet || mongoose.model("wallet", walletSchema);
+export default Dataset;
