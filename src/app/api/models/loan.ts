c8581d8e69f33c18b69fefadd9edc70ad9cb0fdf
Ayodejioladimeji <layobright11@gmail.com>
2025-01-03 01:44:14 +0100

    Implemented loan schemas

---
+++ src/app/api/models/loan.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+
+const loanSchema = new mongoose.Schema(
+    {
+        loanAmount: {
+            type: String,
+            required: true,
+        },
+        repaymentAmount: {
+            type: String,
+            required: true,
+        },
+        interest: {
+            type: String,
+            required: true,
+        },
+        tenure: {
+            type: String,
+            required: true,
+        },
+        purpose: {
+            type: String,
+            required: true,
+        },
+        disbursementDate: {
+            type: String,
+            required: true,
+        },
+        repaymentDate: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            default: "",
+        },
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"user",
+            required:true
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+let Dataset = mongoose.models.loan || mongoose.model("loan", loanSchema);
+export default Dataset;
