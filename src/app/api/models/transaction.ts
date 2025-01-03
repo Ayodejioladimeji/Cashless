8774eb8c39081aedca683eeb7f6c8b1a6472eefa
Ayodejioladimeji <layobright11@gmail.com>
2025-01-03 01:50:10 +0100

    Implemented transaction schema

---
+++ src/app/api/models/transaction.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const transactionSchema = new mongoose.Schema(
+    {
+        recipient: {
+            type: String,
+            required: true,
+        },
+        amount: {
+            type: String,
+            required: true,
+        },
+        reference: {
+            type: String,
+            required: true,
+        },
+        transactionType: {
+            type: String,
+            default: "",
+        },
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"user",
+            required:true
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+let Dataset = mongoose.models.transaction || mongoose.model("transaction", transactionSchema);
+export default Dataset;
