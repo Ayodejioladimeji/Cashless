09cd24a2fa391601d24174d0836d0e6faaba9250
Ayodejioladimeji <layobright11@gmail.com>
2025-01-04 10:00:32 +0100

    Fix linting errors

--- src/app/api/utils/generateToken.ts
+++ src/app/api/utils/generateToken.ts
@@ -1,19 +1,23 @@
 import jwt from "jsonwebtoken";
 
-export const createAccessToken = (payload) => {
-  return jwt.sign(payload, process.env.NEXT_PUBLIC_ACCESS_TOKEN_SECRET, {
+interface TokenPayload {
+  [key: string]: any;
+}
+
+export const createAccessToken = (payload: TokenPayload): string => {
+  return jwt.sign(payload, process.env.NEXT_PUBLIC_ACCESS_TOKEN_SECRET as string, {
     expiresIn: "7d",
   });
 };
 
-export const createRefreshToken = (payload) => {
-  return jwt.sign(payload, process.env.NEXT_PUBLIC_REFRESH_TOKEN_SECRET, {
+export const createRefreshToken = (payload: TokenPayload): string => {
+  return jwt.sign(payload, process.env.NEXT_PUBLIC_REFRESH_TOKEN_SECRET as string, {
     expiresIn: "7d",
   });
 };
 
-export const createActivationToken = (payload) => {
-  return jwt.sign(payload, process.env.NEXT_PUBLIC_ACTIVATION_TOKEN_SECRET, {
+export const createActivationToken = (payload: TokenPayload): string => {
+  return jwt.sign(payload, process.env.NEXT_PUBLIC_ACTIVATION_TOKEN_SECRET as string, {
     expiresIn: "1d",
   });
 };
